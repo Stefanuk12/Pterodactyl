1a7600390cdf0a0a8ce2b922ec7a25bbf7d55d98
Stefanuk12 <42220813+Stefanuk12@users.noreply.github.com>
2022-06-07 20:43:19 +0100

    Changes

--- images/ptero-alpine-wine/entrypoint.sh
+++ images/ptero-alpine-wine/entrypoint.sh
@@ -3,6 +3,10 @@
 # Verify wine install
 wine --version
 
+# Default the TZ environment variable to UTC.
+TZ=${TZ:-UTC}
+export TZ
+
 # Make internal Docker IP address available to processes.
 INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
 export INTERNAL_IP
@@ -57,6 +61,9 @@ do
     echo "Installing $trick"
     winetricks -q $trick
 done
+
+# Switch to the container's working directory
+cd /home/container || exit 1
 
 # Replace Startup Variables
 MODIFIED_STARTUP=$(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
