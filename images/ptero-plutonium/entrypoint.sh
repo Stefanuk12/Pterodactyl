83f8d4194fdfa49b5428dca808e2672b5ec98476
Stefanuk12 <42220813+Stefanuk12@users.noreply.github.com>
2022-06-12 21:19:17 +0100

    New image

---
+++ images/ptero-plutonium/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+cd /home/container
+
+# Installing bootstrapper / updater
+wget https://github.com/mxve/plutonium-updater.rs/releases/latest/download/plutonium-updater-x86_64-unknown-linux-gnu.tar.gz -O latestupdater.tar.gz -q --show-progress
+tar -xvf latestupdater.tar.gz -C localappdata/Plutonium
+rm latestupdater.tar.gz
+chmod +x localappdata/Plutonium/plutonium-updater
+./localappdata/Plutonium/plutonium-updater -d localappdata/Plutonium
+rm localappdata/Plutonium/plutonium-updater
+
+# Replace Startup Variables
+MODIFIED_STARTUP=$(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+echo ":/home/container$ ${MODIFIED_STARTUP}"
+
+# Run the Server
+eval ${MODIFIED_STARTUP}
