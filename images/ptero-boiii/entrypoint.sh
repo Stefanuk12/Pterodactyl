5be4e7afba5972863d2cdffc346944597eb6da25
Stefanuk12 <42220813+Stefanuk12@users.noreply.github.com>
2023-05-17 16:13:49 +0100

    Initial commit for boiii

---
+++ images/ptero-boiii/entrypoint.sh
@@ -0,0 +1,6 @@
+# Replace Startup Variables
+MODIFIED_STARTUP=$(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+echo ":/home/container$ ${MODIFIED_STARTUP}"
+
+# Run the Server
+eval ${MODIFIED_STARTUP}
