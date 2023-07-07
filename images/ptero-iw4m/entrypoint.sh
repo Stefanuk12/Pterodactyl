a934bc18ceae49c14de61f95e29b53f010df9565
Stefanuk12 <42220813+Stefanuk12@users.noreply.github.com>
2023-07-07 16:33:33 +0100

    Attempt to fix?

--- images/ptero-iw4m/entrypoint.sh
+++ images/ptero-iw4m/entrypoint.sh
@@ -1,9 +1,11 @@
+#!/bin/bash
+cd /home/container
+
 # Make internal Docker IP address available to processes.
-INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
-export INTERNAL_IP
+export INTERNAL_IP=`ip route get 1 | awk '{print $(NF-2);exit}'`
 
 # Replace Startup Variables
-MODIFIED_STARTUP=$(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
